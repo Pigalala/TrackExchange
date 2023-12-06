88d1fce42c41f6a4101113a09347808b99bb4f9f
Pigalala ‎ <w8888mc@gmail.com>
2023-12-06 12:54:24 +0000

    init

---
+++ src/main/java/me/pigalala/trackexchange/TrackExchange.java
@@ -0,0 +1,14 @@
+package me.pigalala.trackexchange;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TrackExchange extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
