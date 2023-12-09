4dd94d2d40d6e022954e0c081fba6c2bf754f95e
Pigalala ‎ <w8888mc@gmail.com>
2023-12-09 14:46:33 +0000

    Remove unused classes

--- src/main/java/me/pigalala/trackexchange/exceptions/TrackExchangeException.java
+++
@@ -1,8 +0,0 @@
-package me.pigalala.trackexchange.exceptions;
-
-public abstract class TrackExchangeException extends Exception {
-
-    public TrackExchangeException(String message) {
-        super(message);
-    }
-}
--- src/main/java/me/pigalala/trackexchange/exceptions/TrackFileAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package me.pigalala.trackexchange.exceptions;
-
-public class TrackFileAlreadyExistsException extends TrackExchangeException {
-
-    public TrackFileAlreadyExistsException() {
-        super("A TrackExchange file with this name already exists");
-    }
-}
