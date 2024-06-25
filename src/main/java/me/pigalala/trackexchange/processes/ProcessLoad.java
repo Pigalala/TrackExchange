eeda10886be0925a2b7c7f9d01fc61a1922f7e70
Pigalala <73348859+Pigalala@users.noreply.github.com>
2024-06-25 17:53:57 +0100

    26 schematic partitioning part 6 but it doesn't do as the title says (#30)
    * Update pom
    
    * Make WE a dependency
    
    * Bruh
    
    * Undoing pastes (#29)
    
    * Unsilly

--- src/main/java/me/pigalala/trackexchange/processes/ProcessLoad.java
+++ src/main/java/me/pigalala/trackexchange/processes/ProcessLoad.java
@@ -1,17 +1,24 @@
 package me.pigalala.trackexchange.processes;
 
 import co.aikar.taskchain.TaskChain;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import me.makkuusen.timing.system.database.TrackDatabase;
+import me.makkuusen.timing.system.track.Track;
 import me.pigalala.trackexchange.TrackExchange;
 import me.pigalala.trackexchange.trackcomponents.TrackExchangeFile;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Stack;
 import java.util.concurrent.CompletableFuture;
 
 public class ProcessLoad extends Process {
@@ -37,13 +44,17 @@ public class ProcessLoad extends Process {
         notifyProcessStartText();
 
         chain.async(this::doReadStage)
-                .asyncFutures((f) -> List.of(CompletableFuture.supplyAsync(this::doTrackStage), CompletableFuture.supplyAsync(this::doPasteStage)))
-                .execute((success) -> {
-                    if (success)
-                        notifyProcessFinishText(System.currentTimeMillis() - startTime);
-                    else
-                        notifyProcessFinishExceptionallyText();
+            .asyncFutures((f) -> List.of(
+                    CompletableFuture.supplyAsync(this::doTrackStage),
+                    CompletableFuture.supplyAsync(this::doPasteStage)
+                    )).execute((success) -> {
+                if (success) {
+                    createUndo();
+                    notifyProcessFinishText(System.currentTimeMillis() - startTime);
+                } else {
+                    notifyProcessFinishExceptionallyText();
                 }
+            }
         );
     }
 
@@ -69,7 +80,8 @@ public class ProcessLoad extends Process {
         notifyStageBeginText(stage);
         TrackExchangeFile trackExchangeFile = chain.getTaskData("trackExchangeFile");
         try {
-            trackExchangeFile.getTrack().createTrack(player);
+            Track track = trackExchangeFile.getTrack().createTrack(player);
+            chain.setTaskData("track", track);
             notifyStageFinishText(stage, System.currentTimeMillis() - startTime);
         } catch (SQLException e) {
             notifyStageFinishExceptionallyText(stage, e);
@@ -87,7 +99,8 @@ public class ProcessLoad extends Process {
         trackExchangeFile.getSchematic().ifPresentOrElse(schematic -> {
             notifyStageBeginText(stage);
             try {
-                schematic.pasteAt(origin);
+                EditSession session = schematic.pasteAt(origin);
+                chain.setTaskData("editSession", session);
                 notifyStageFinishText(stage, System.currentTimeMillis() - startTime);
             } catch (WorldEditException e) {
                 notifyStageFinishExceptionallyText(stage, e);
@@ -97,5 +110,24 @@ public class ProcessLoad extends Process {
         });
 
         return null;
+    }
+
+    private void createUndo() {
+        Runnable undoAction = () -> {
+            Track track = chain.getTaskData("track");
+            EditSession session = chain.getTaskData("editSession");
+            player.sendMessage(Component.text("Undoing track " + track.getDisplayName() + ".", NamedTextColor.YELLOW));
+            if (session != null) {
+                Bukkit.getScheduler().runTaskAsynchronously(TrackExchange.instance, () -> {
+                    try (EditSession undoSession = WorldEdit.getInstance().newEditSession(BukkitAdapter.adapt(player))) {
+                        session.undo(undoSession);
+                    }
+                });
+            }
+            TrackDatabase.removeTrack(track);
+        };
+
+        TrackExchange.playerActions.putIfAbsent(player.getUniqueId(), new Stack<>());
+        TrackExchange.playerActions.get(player.getUniqueId()).push(undoAction);
     }
 }
