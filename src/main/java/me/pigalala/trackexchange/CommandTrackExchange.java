4cd5ab3ce06564918ae5190cb83e3d86e591905a
Pigalala <w8888mc@gmail.com>
2025-04-24 18:01:15 +0100

    Improve some error handling

--- src/main/java/me/pigalala/trackexchange/CommandTrackExchange.java
+++ src/main/java/me/pigalala/trackexchange/CommandTrackExchange.java
@@ -18,14 +18,16 @@ public class CommandTrackExchange extends BaseCommand {
     @CommandCompletion("@track <saveas>")
     @CommandPermission("trackexchange.export")
     public static void onCopy(Player player, Track track, @Optional @Single String saveAs) {
-        if(saveAs == null)
+        if (saveAs == null) {
             saveAs = track.getCommandName();
-
-        if(TrackExchangeFile.trackExchangeFileExists(saveAs))
+        }
+        if (!TrackExchange.isButlerEnabled() && TrackExchangeFile.trackExchangeFileExists(saveAs)) {
             throw new ConditionFailedException("This trackexchange file already exists");
+        }
 
-        if(!saveAs.matches("[A-Za-z0-9_]+"))
+        if (!saveAs.matches("[A-Za-z0-9_]+")) {
             throw new ConditionFailedException("You cannot save a trackexchange track with that name");
+        }
 
         new ProcessSave(player, track, saveAs).execute();
     }
@@ -35,17 +37,20 @@ public class CommandTrackExchange extends BaseCommand {
     @CommandPermission("trackexchange.import")
     public static void onPaste(Player player, String fileName, @Optional String loadAs) {
         fileName = fileName.replace(".trackexchange", "");
-        if(loadAs == null)
+        if (loadAs == null) {
             loadAs = fileName;
+        }
 
-//        if(!TrackExchangeFile.trackExchangeFileExists(fileName))
-//            throw new ConditionFailedException("This trackexchange file does not exist");
+        if (!TrackExchange.isButlerEnabled() && !TrackExchangeFile.trackExchangeFileExists(fileName))
+            throw new ConditionFailedException("This trackexchange file does not exist");
 
-        if(TrackDatabase.trackNameNotAvailable(loadAs))
+        if (TrackDatabase.trackNameNotAvailable(loadAs)) {
             throw new ConditionFailedException("A track with this name already exists");
+        }
 
-        if(!loadAs.matches("[A-Za-z0-9 ]+"))
+        if (!loadAs.matches("[A-Za-z0-9 ]+")) {
             throw new ConditionFailedException("You cannot load a trackexchange track with that name");
+        }
 
         if(ApiUtilities.checkTrackName(loadAs))
             throw new ConditionFailedException("That name is not legal");
